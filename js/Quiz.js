f0700ab3b3872a33ab1e2339ef45b735f15a6009
HartejAnand <76600578+HartejAnand@users.noreply.github.com>
2021-04-09 15:18:50 -0400

    Add files via upload

---
+++ js/Quiz.js
@@ -0,0 +1,65 @@
+class Quiz {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      contestant = new Contestant();
+      var contestantCountRef = await database.ref('contestantCount').once("value");
+      if(contestantCountRef.exists()){
+        contestantCount = contestantCountRef.val();
+        contestant.getCount();
+      }
+      question = new Question()
+      question.display();
+    }
+  }
+
+  play(){
+    //write code here to hide question elements
+    question.hide();
+    //write code to change the background color here
+    background(0,0,155);
+    //write code to show a heading for showing the result of Quiz
+    var results=createElement('h2');
+    results.html("  RESULTS");
+    //call getContestantInfo( ) here
+    if(allConstraints !== undefined){
+      fill(255,0,255);
+      textSize(30);
+      text("correct answers in green",130,230);
+    }
+
+    //write condition to check if contestantInfor is not undefined
+    for(var plr in allConstraints){
+      var correct =2;
+      if(correct===allConstraints[plr].answer){
+        fill(0,255,0);
+      }
+      else{
+        fill(255,155,0);
+      }
+      display_position+=30
+        text(allConstraints[plr].name,250,display_position);
+      
+    }
+    //write code to add a note here
+
+    //write code to highlight contest who answered correctly
+    
+  }
+
+}
