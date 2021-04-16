bfb2ee4ef23d30a34132e56e4c7a0f4a33febdc6
HartejAnand <76600578+HartejAnand@users.noreply.github.com>
2021-04-16 16:08:02 -0400

    Add files via upload

--- js/Quiz.js
+++ js/Quiz.js
@@ -35,29 +35,35 @@ class Quiz {
     //write code to change the background color here
     background(100,100,255);
     //write code to show a heading for showing the result of Quiz
-    var results=createElement('h2');
-    results.html("  RESULTS");
+    //var results=createElement('h2');
+    //results.html("  RESULTS");
+    textSize(40);
+    fill(255,155,0);
+    text("results",250,300);
     //call getContestantInfo( ) here
     Contestant.getPlayerInfo( );
     //write condition to check if contestantInfor is not undefined
-    if(allConstraints !== undefined){
+    if(allContestants !== undefined){
       fill(255,0,255);
       textSize(30);
     }
+    var display_position=320;
     //write code to add a note here
           text("correct answers in green",130,230);
     //write code to highlight contest who answered correctly
-    for(var plr in allConstraints){
-      var correct =2;
-      if(correct===allConstraints[plr].answer){
+    for(var plr in allContestants){
+      var correct ="2";
+      if(correct===allContestants[plr].answer){
         fill(0,255,0);
+        console.log("green");
       }
-      else{
-        fill(255,155,0);
+      else if(correct!==allContestants[plr].answer){
+        fill(255,0,0);
+        console.log("red");
       }
       display_position+=30;
-        //text(allConstraints[plr].name,250,display_position);
-        text("testing",250,display_position);
+        text(allContestants[plr].name+" ↣ "+allContestants[plr].answer,50,display_position);
+        //text("testing",250,display_position);
       
     }
   }
